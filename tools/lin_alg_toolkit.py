a4edb65985a7b6a20f87a3eac9858fde09476f71
Frederik Høngaard <mail@frederikhoengaard.com>
2020-07-21 23:09:45 +0200

    added invert_matrix to lin_alg_toolkit

--- tools/lin_alg_toolkit.py
+++ tools/lin_alg_toolkit.py
@@ -156,12 +156,50 @@ def reduced_row_echelon(augmented_matrix):
 
 
 
+def invert_matrix(matrix):
+    """
+
+    """
+    m = len(matrix)
+    n = len(matrix[0])
+    if m != n: # nonsquare matrices do not have inverses
+        return
+    identity_matrix = create_identity_matrix(m)
+    
+    # only matrices with exactly one solution are invertible, so lets use our gauss-jordan script
+    reduced_matrix = [row for row in matrix]
+    reduced_matrix = reduced_row_echelon(reduced_matrix)
+    
+    for row in reduced_matrix: # check for zero-row
+        invertable = False
+        for value in row:
+            if value != 0:
+                invertable = True
+        if not invertable:
+            print('not invertable')
+            return
+    
+    adjoined_matrix = []
+    for i in range(m):
+        tmp = [val for val in matrix[i]]
+        tmp.extend(identity_matrix[i])
+        adjoined_matrix.append(tmp)
+
+    reduced_adjoined_matrix = reduced_row_echelon(adjoined_matrix)
+    inverted_matrix = []
+    for i in range(m):
+        inverted_matrix.append([reduced_adjoined_matrix[i][j] for j in range(n,(2*n))])
+    return inverted_matrix
+
 
 
 def main():
-    A = read_matrix('input3.txt')
-    for line in reduced_row_echelon(A):
+    A = read_matrix('invert-1.txt')
+    for line in A:
         print(line)
+    if invert_matrix(A):
+        for line in invert_matrix(A):
+            print(line)
 
 
 
