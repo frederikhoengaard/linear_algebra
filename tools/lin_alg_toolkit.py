08ad101001143282d8bf35b9135b1f7cbeb1eaa6
Frederik Høngaard <mail@frederikhoengaard.com>
2020-07-21 22:54:29 +0200

    restructured repo

--- tools/lin_alg_toolkit.py
+++ tools/lin_alg_toolkit.py
@@ -105,12 +105,60 @@ def create_identity_matrix(order: int):
     return out
 
 
+def reduced_row_echelon(augmented_matrix):
+    m,n = [len(augmented_matrix),len(augmented_matrix[0])]
+    n_variables = n - 1
+    evaluated_rows = []
+
+    for i in range(n_variables):
+        maxrow = 0
+        maxval = 0
+
+        for j in range(m):
+            if (abs(augmented_matrix[j][i]) > abs(maxval)) and j not in evaluated_rows:
+                maxrow = j
+                maxval = augmented_matrix[j][i]
+        evaluated_rows.append(maxrow)
+
+        if maxval == 0:
+            continue
+
+        other_rows = [row for row in range(m) if row != maxrow]
+        reciprocal = 1 / augmented_matrix[maxrow][i]
+        new_row = [coefficient * reciprocal for coefficient in augmented_matrix[maxrow]]
+
+        augmented_matrix[maxrow] = new_row
+
+        for row_num in other_rows:
+            multiplier = augmented_matrix[row_num][i]
+            new_other_row = [augmented_matrix[row_num][k] - (multiplier * new_row[k]) for k in range(n)]
+            augmented_matrix[row_num] = new_other_row
+
+    augmented_matrix = tidy_up(augmented_matrix)
+
+    if n_variables > m:
+        n_exchanges = m
+    else:
+        n_exchanges = n_variables
+
+    for variable in range(n_exchanges):
+        for i in range(m):
+            if (augmented_matrix[i][variable] != 0) and (i != variable):
+                tmp = augmented_matrix[variable]
+                augmented_matrix[variable] = augmented_matrix[i]
+                augmented_matrix[i] = tmp
+                break
+   
+    return augmented_matrix
+
+
 
 
 
 def main():
-    A = read_matrix('transpose_3.txt')
-    for line in transpose_matrix(A):
+    A = read_matrix('A3.txt')
+    B = read_matrix('B3.txt')
+    for line in dot_product(A,B):
         print(line)
 
 
